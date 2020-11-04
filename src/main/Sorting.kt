a423f145752b0cabf66671c320e322d47c0944d3
hafidhadhi <hafidh.adhi@gmail.com>
2020-11-04 23:22:07 +0700

    adding bubble sort

--- src/main/Sorting.kt
+++ src/main/Sorting.kt
@@ -1,7 +1,6 @@
 fun main(args: Array<String>) {
-    val input = intArrayOf(4, 9, 7, 5, 8, 9, 3, 10, 1)
-    input.quickSort()
-    println(input.toList().toString())
+    val input = intArrayOf(4, 9, 7, 5, 8, 9, 3)
+    input.bubbleSort()
 }
 
 fun IntArray.partition(begin: Int, end: Int): Int {
@@ -27,4 +26,27 @@ fun IntArray.quickSort(begin: Int = 0, end: Int = size - 1) {
     val pivot = partition(begin, end)
     quickSort(begin, pivot - 1)
     quickSort(pivot + 1, end)
+}
+
+fun IntArray.bubbleSort() {
+    var sorted = false
+    var temp: Int
+    var swap = 0
+    while (!sorted) {
+        sorted = true
+        for (i in 0 until size - 1) {
+            if (this[i] > this[i + 1]) {
+                swap++
+                temp = this[i]
+                this[i] = this[i + 1]
+                this[i + 1] = temp
+                sorted = false
+                print("${swap}. [${this[i]}, ${this[i + 1]}] -> ")
+                this.forEach { print("$it ") }
+                println()
+                break
+            }
+        }
+    }
+    println("\nJumlah swap: $swap")
 }
